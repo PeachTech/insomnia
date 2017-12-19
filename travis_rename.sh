ee446fbaae9ca1afc7c82a62f38c31f4db6c4a70
Jeff Tucker <j.tucker@peach.tech>
2017-12-19 14:34:49 -0800

    dev: mac and linux installer names should now be correct

--- travis_rename.sh
+++ travis_rename.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 ver=$(node -e "console.log(require('./app/package.json').version);")
 PATH=`echo $PATH | sed -e 's/:\.\/node_modules\/\.bin//'`;
-find ./dist -type f -name *.AppImage -execdir mv {} peach_insomnia_setup-$ver-x86_64-linux.AppImage \;
-find ./dist -type f -name *.dmg -execdir mv {} peach_insomnia_setup-$ver-x86_64-mac.dmg \;
+find ./dist -type f -name *.AppImage -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
+find ./dist -type f -name *.dmg -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
