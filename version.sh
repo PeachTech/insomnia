2f8b7add7480d4f080c978ecd5a280aacf482efa
Jeff Tucker <j.tucker@peach.tech>
2018-01-18 16:39:45 -0800

    dev: fixed version number with tag so that it actually works

--- version.sh
+++ version.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 echo "will set version on package.json to latest tag"
-# lock the version to the latest git tag
-npm version from-git --no-git-tag-version --allow-same-version
-# now we need to apply this to app/package.json
-# ironically, 'npm version' outputs the version number but not in the format
-# that we need, so just read the file we just changed
-ver=$(node -e "console.log(require('./package.json').version);")
-cd app
-npm version $ver --no-git-tag-version --allow-same-version
+if [ -z "$TRAVIS_TAG" ]; then
+    exit 0;
+else
+    # lock the version to the latest git tag
+    npm version from-git --no-git-tag-version --allow-same-version --force
+    # now we need to apply this to app/package.json
+    # ironically, 'npm version' outputs the version number but not in the format
+    # that we need, so just read the file we just changed
+    ver=$(node -e "console.log(require('./package.json').version);")
+    cd app
+    npm version $ver --no-git-tag-version --allow-same-version --force
+fi
