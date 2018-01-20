d7a45e86165db70fd698f7bfd371d09f828fe391
Jeff Tucker <j.tucker@peach.tech>
2018-01-19 17:45:51 -0800

    dev: version.sh no longer allows npm to try to figure out the version on its own

--- version.sh
+++ version.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 echo "will set version on package.json to latest tag"
 # lock the version to the latest git tag
-npm version from-git --no-git-tag-version --allow-same-version --force
+# can't actually use "from-git" due to repo not being clean and it not respecting --no-git-tag-version
+# it should work, but it errors every time
+ver=$(git describe --abbrev=0)
+echo "ver has been set to $ver"
+npm version $ver --no-git-tag-version --allow-same-version --force
 # now we need to apply this to app/package.json
 # ironically, 'npm version' outputs the version number but not in the format
 # that we need, so just read the file we just changed
-ver=$(node -e "console.log(require('./package.json').version);")
-echo "ver has been set to $ver"
 cd app
 npm version $ver --no-git-tag-version --allow-same-version --force
