f952d6c2dab1d493178dd0a21f1702918494d5c3
Jeff Tucker <j.tucker@peach.tech>
2018-01-19 17:48:15 -0800

    dev: fixed renaming of installers so that it isn't broken anymore but deploy is still going to be broken if a new tag is pushed

--- travis_rename.sh
+++ travis_rename.sh
@@ -6,5 +6,6 @@ mkdir -p ./dist/deploy
 result=$?
 echo "result of mkdir -p ./dist/deploy was $result"
 echo "working directory is currently $(pwd)"
-find ./dist -type f -name *.AppImage -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
-find ./dist -type f -name *.dmg -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
+ls
+find ./dist -type f -name *.AppImage -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
+find ./dist -type f -name *.dmg -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
