916ba6f19657d04ce5a916a45c1ea0a0d2c4261f
Jeff Tucker <j.tucker@peach.tech>
2018-01-19 17:28:38 -0800

    dev: hopefully fixed travis_rename script to create correct directory

--- travis_rename.sh
+++ travis_rename.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
 ver=$(node -e "console.log(require('./app/package.json').version);")
 PATH=`echo $PATH | sed -e 's/:\.\/node_modules\/\.bin//'`;
+echo "making ./dist/deploy"
 mkdir -p ./dist/deploy
+result=$?
+echo "result of mkdir -p ./dist/deploy was $result"
+echo "working directory is currently $(pwd)"
 find ./dist -type f -name *.AppImage -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
 find ./dist -type f -name *.dmg -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
