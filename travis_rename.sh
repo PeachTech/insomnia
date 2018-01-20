a17043c6d1e07e5125478dc67e2fbb8abe6bedc9
Jeff Tucker <j.tucker@peach.tech>
2018-01-19 17:00:51 -0800

    dev: fixed travis to move artifacts to dist/deploy to avoid deploying the whole dist folder

--- travis_rename.sh
+++ travis_rename.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 ver=$(node -e "console.log(require('./app/package.json').version);")
 PATH=`echo $PATH | sed -e 's/:\.\/node_modules\/\.bin//'`;
-find ./dist -type f -name *.AppImage -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
-find ./dist -type f -name *.dmg -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
+mkdir -p ./dist/deploy
+find ./dist -type f -name *.AppImage -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
+find ./dist -type f -name *.dmg -execdir mv {} ./dist/deploy/Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
