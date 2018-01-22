9f909360bb80a71216aa714027b6a2001dd712bd
Jeff Tucker <j.tucker@peach.tech>
2018-01-22 11:28:09 -0800

    dev: fixed upload for travis ci deploy step, temporarily commented out tags

--- travis_rename.sh
+++ travis_rename.sh
@@ -6,6 +6,12 @@ mkdir -p ./dist/deploy
 result=$?
 echo "result of mkdir -p ./dist/deploy was $result"
 echo "working directory is currently $(pwd)"
-ls
+ls ./dist/deploy
+# must use execdir in order to actually rename the file or it will want the full path
+# in the command
 find ./dist -type f -name *.AppImage -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-linux.AppImage \;
 find ./dist -type f -name *.dmg -execdir mv {} Peach-Insomnia-Setup-$ver-x86_64-mac.dmg \;
+# copy to dist/deploy so that upload-dir in the deploy provider will only copy 
+# the files we actually want
+find ./dist -type f -name Peach*.AppImage -exec mv {} ./dist/deploy/ \;
+find ./dist -type f -name Peach*.dmg -exec mv {} ./dist/deploy/ \;
