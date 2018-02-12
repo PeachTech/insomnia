4c562b3f5d5d7f012893ea0e19717833a1b26706
Jeff Tucker <j.tucker@peach.tech>
2018-02-12 12:13:07 -0800

    dev: removed google analytics calls

--- app/analytics/index.js
+++ app/analytics/index.js
@@ -1,7 +1,6 @@
-import * as google from './google';
 import * as models from '../models';
 import {ipcRenderer} from 'electron';
-import {getAppVersion, getAppPlatform, isDevelopment} from '../common/constants';
+import {isDevelopment} from '../common/constants';
 
 let initialized = false;
 export async function init (accountId) {
@@ -15,8 +14,6 @@ export async function init (accountId) {
   if (initialized) {
     return;
   }
-
-  await google.init(accountId, getAppPlatform(), getAppVersion());
 
   initialized = true;
 
@@ -39,14 +36,8 @@ export async function init (accountId) {
 
 export function trackEvent (...args) {
   // Do on next tick in case it fails or blocks
-  process.nextTick(() => {
-    google.sendEvent(...args);
-  });
 }
 
 export function setAccountId (accountId) {
   // Do on next tick in case it fails or blocks
-  process.nextTick(() => {
-    google.setUserId(accountId);
-  });
 }
