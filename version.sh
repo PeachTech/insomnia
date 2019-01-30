64e85ec60230d8606e197c3167ae166eb5683eff
Jeff Tucker <j.tucker@peach.tech>
2019-01-30 12:38:48 -0800

    dev: Fixed versioning script used for travis-ci builds.

--- version.sh
+++ version.sh
@@ -9,5 +9,5 @@ npm version $ver --no-git-tag-version --allow-same-version --force
 # now we need to apply this to app/package.json
 # ironically, 'npm version' outputs the version number but not in the format
 # that we need, so just read the file we just changed
-cd app
+cd packages/insomnia-app
 npm version $ver --no-git-tag-version --allow-same-version --force
