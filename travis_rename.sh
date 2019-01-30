dd033dad1194c5d966416c036f5eddaa4260a0c3
Jeff Tucker <j.tucker@peach.tech>
2019-01-30 15:00:55 -0800

    dev: Attempting to fix file copy into correct destination.

--- travis_rename.sh
+++ travis_rename.sh
@@ -7,25 +7,27 @@ mkdir -p ./dist/deploy
 result=$?
 echo "result of mkdir -p ./dist/deploy was $result"
 echo "working directory is currently $(pwd)"
+deploydir=$(pwd)/dist/deploy
 ls ./dist/deploy
+echo "deploydir will be $deploydir"
 # must use execdir in order to actually rename the file or it will want the full path
 # in the command
 echo "renaming installer"
-find ./packages/insomnia-app/dist -type f -name *.AppImage -execdir cp {} Peach-Sidecar-Setup-$ver-x86_64-linux.AppImage \;
-find ./packages/insomnia-app/dist -type f -name *.snap -execdir cp {} Peach-Sidecar-Setup-$ver-x86_64-linux.snap \;
-find ./packages/insomnia-app/dist -type f -name *.dmg -execdir cp {} Peach-Sidecar-Setup-$ver-x86_64-mac.dmg \;
+find ./packages/insomnia-app/dist -type f -name *.AppImage -execdir cp {} $deploydir/Peach-Sidecar-Setup-$ver-x86_64-linux.AppImage \;
+find ./packages/insomnia-app/dist -type f -name *.snap -execdir cp {} $deploydir/Peach-Sidecar-Setup-$ver-x86_64-linux.snap \;
+find ./packages/insomnia-app/dist -type f -name *.dmg -execdir cp {} $deploydir/Peach-Sidecar-Setup-$ver-x86_64-mac.dmg \;
 echo "installer renamed"
 # copy to dist/deploy so that upload-dir in the deploy provider will only copy 
 # the files we actually want
 # echo "copying AppImage to ./dist/deploy"
-find ./packages/insomnia-app/dist -type f -name Peach*.AppImage -exec mv -f {} ./dist/deploy/ \;
+# find ./packages/insomnia-app/dist -type f -name Peach*.AppImage -exec mv -f {} ./dist/deploy/ \;
 # echo "AppImage copied."
 # ls -al ./dist/deploy
 # echo "copying dmg to ./dist/deploy"
-find ./packages/insomnia-app/dist -type f -name Peach*.dmg -exec mv -f {} ./dist/deploy/ \;
+# find ./packages/insomnia-app/dist -type f -name Peach*.dmg -exec mv -f {} ./dist/deploy/ \;
 # echo "dmg copied"
 # echo "copying snap to ./dist/deploy"
-find ./packages/insomnia-app/dist -type f -name Peach*.snap -exec mv -f {} ./dist/deploy/ \;
+# find ./packages/insomnia-app/dist -type f -name Peach*.snap -exec mv -f {} ./dist/deploy/ \;
 # echo "dmg copied"
 echo "Contents of dist/deploy folder are:"
 ls -al ./dist/deploy
