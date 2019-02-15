c93dd02461735953e3d16179ef0b16f49e8a8c9d
Jeff Tucker <j.tucker@peach.tech>
2019-02-15 15:58:01 -0800

    dev: Added aws region, fixed directory to deploy filenames and not dist/deploy path to bucket.

--- upload_artifacts.sh
+++ upload_artifacts.sh
@@ -18,10 +18,11 @@ echo "starting upload script"
 echo Using insomnia version $INSOMNIA_VERSION 
 INSOMNIA_VERSION=$(node -e "console.log(require('./package.json').version);")
 echo insomnia version is now $INSOMNIA_VERSION
-echo would upload from artifacts paths: $ARTIFACTS_PATHS
-echo Artifacts working dir is: $ARTIFACTS_WORKING_DIR
 echo "if the above is blank, something is wrong"
-UPLOADFILES=$(find ./dist/deploy -type f -name *.AppImage -o -name *.dmg -o -name *.snap | tr "\n" ":")
+pushd ./dist/deploy
+UPLOADFILES=$(find . -type f -name *.AppImage -o -name *.dmg -o -name *.snap | tr "\n" ":")
 echo "files to upload are $UPLOADFILES"
+ARTIFACTS_REGION=us-west-1
 artifacts -D upload --target-paths "release/1.5/$INSOMNIA_VERSION" $UPLOADFILES
-echo "Upload would have been completed here"
+popd
+echo "Upload should be complete."
