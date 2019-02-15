b12834cb38a3137806fcddd4e276881d0bec58a9
Jeff Tucker <j.tucker@peach.tech>
2019-02-15 15:29:15 -0800

    dev: Attempting to upload actual artifacts to correct directory.

--- upload_artifacts.sh
+++ upload_artifacts.sh
@@ -8,6 +8,5 @@ echo Artifacts working dir is: $ARTIFACTS_WORKING_DIR
 echo "if the above is blank, something is wrong"
 UPLOADFILES=$(find ./dist/deploy -type f -name *.AppImage -o -name *.dmg -o -name *.snap | tr "\n" ":")
 echo "files to upload are $UPLOADFILES"
-# artifacts -D upload --target-paths /release/1.5/$INSOMNIA_VERSION
+artifacts -D upload --target-paths "release/1.5/$INSOMNIA_VERSION" $UPLOADFILES
 echo "Upload would have been completed here"
-
