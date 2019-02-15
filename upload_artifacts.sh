77a8adbf2978ef8876ba1a1fa55ac47de732cb94
Jeff Tucker <j.tucker@peach.tech>
2019-02-15 14:54:58 -0800

    dev: Fixed settings for script deploy.

--- upload_artifacts.sh
+++ upload_artifacts.sh
@@ -6,9 +6,8 @@ echo insomnia version is now $INSOMNIA_VERSION
 echo would upload from artifacts paths: $ARTIFACTS_PATHS
 echo Artifacts working dir is: $ARTIFACTS_WORKING_DIR
 echo "if the above is blank, something is wrong"
-echo $(find dist -type f -name *.AppImage -o -name *.dmg -o -name *.snap | tr "\n" ":")
-echo "environment is:"
-env
+UPLOADFILES=$(find ./dist/deploy -type f -name *.AppImage -o -name *.dmg -o -name *.snap | tr "\n" ":")
+echo "files to upload are $UPLOADFILES"
 # artifacts -D upload --target-paths /release/1.5/$INSOMNIA_VERSION
 echo "Upload would have been completed here"
 
